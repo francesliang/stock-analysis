a39a3e1deb1cd42e26ab1e6a1e9ce4318a50fc8f
Xin <frances.xliang@gmail.com>
2020-05-02 21:29:43 +1000

    Add function to get valuation by category

--- main.go
+++ main.go
@@ -7,18 +7,37 @@ import (
     "io/ioutil"
 )
 
-func main() {
-    var categoryJsonFile = "stock-categories.json"
+
+func generateCategoryMap(categoryJsonFile string) {
     categories := Fmp.GetCategoryMap(categoryJsonFile)
     categoriesJson, _ := json.MarshalIndent(categories, "", "   ")
     ioutil.WriteFile(categoryJsonFile, categoriesJson, 0644)
     fmt.Println(string(categoriesJson))
+}
+
+func CategoryValuation(categoryJsonFile string, sector string, industry string) {
+    var categories = make(map[string]map[string][]string)
+    data, _ := ioutil.ReadFile(categoryJsonFile)
+    json.Unmarshal(data, &categories)
+
+    symbols := categories[sector][industry]
+
+    hComparison := Fmp.HorizontalBasicValuation(symbols)
+    hComparisonJson, _ := json.MarshalIndent(hComparison, "", "   ")
+    fmt.Println(string(hComparisonJson))
+
+}
+
+func main() {
+    var categoryJsonFile = "stock-categories.json"
+    //generateCategoryMap(categoryJsonFile)
+
+    var sector = "Technology"
+    var industry = "Application Software"
+    CategoryValuation(categoryJsonFile, sector, industry)
 
     //vComparison := Fmp.VerticalBasicValuation("AAPL")
     //vComparisonJson, _ := json.MarshalIndent(vComparison, "", "   ")
     //fmt.Println(string(vComparisonJson))
 
-    //hComparison := Fmp.HorizontalBasicValuation([]string{"AAPL", "GOOG"})
-    //hComparisonJson, _ := json.MarshalIndent(hComparison, "", "   ")
-    //fmt.Println(string(hComparisonJson))
 }
